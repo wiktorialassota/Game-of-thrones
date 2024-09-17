37483f5ecce7f3ea5f17449a8afac1df350aea3c
Wiktoria Lassota <wiktorialassota@gmail.com>
2024-09-17 15:15:53 +0100

    Fixing live browser issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
-import HomePage from './pages/homepage';
-import Navbar from './components/navbar';
-import CharacterDetailPage from './pages/characterDetailPage';
+import {Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homepage";
+import Navbar from "./components/navbar";
+import CharacterDetailPage from "./pages/characterDetailPage";
 
 const App = () => {
   return (
-      <div>
-        <Navbar />
+    <div>
+      <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/character/:id" element={<CharacterDetailPage />} />
         </Routes>
-      </div>
+    </div>
   );
 };
 
